622703001e796e8e3979e5ad18160d3e88883747
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2021-03-09 21:51:13 -0300

    bible api fix v3

--- src/pages/api/biblia.ts
+++ src/pages/api/biblia.ts
@@ -1,20 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import BibleData from '../../../public/assets/the-message-the-bible-ptbr.json'
 
-const Bible = (req: NextApiRequest, res: NextApiResponse) => {
+const Bible = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') {
     res.status(500).json({message: 'Desculpe, só aceitamos solicitações GET '});
   } else {
+    const BibleData = await import('../../../public/assets/the-message-the-bible-ptbr.json');
     res.status(200).json(BibleData);
   }
 }
 
 export const config = {
   api: {
-    externalResolver: true,
-    bodyParser: false,
+    bodyParser: {
+      sizeLimit: '5mb',
+    },
   },
 }
 
 export default Bible
+
