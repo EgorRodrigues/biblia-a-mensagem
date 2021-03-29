4d041b80a41fd2b96b1253cc87807e460fcd4418
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2021-03-29 08:36:06 -0300

    added google tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,25 +12,39 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/icons/bible.svg" type="image/png"/>
           <meta name="google-site-verification" content="rHQeqL_skLqXXGPdwGF-v0sQS3-A8d4VxzQ8kOM0wYY" />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
+          <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
-            });
-          `,
+            });`,
             }}
           />
+
+          {/* <!-- Google Tag Manager --> */}
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KQ9HN6G');`,
+          }}/>
+          {/* <!-- End Google Tag Manager --> */}
         </Head>
+
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ9HN6G" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
